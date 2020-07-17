87bc36bc7b2568eb4777077f9799f10205ecfebe
zhili.zhang <512929087@qq.com>
2020-07-17 18:57:32 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Todos</h3>
+	<div class="info">
+		<input type="text" class="write" ref='write'/>
+		<button class="btn" @click="getwrite">提交</button>
+	</div>
+	<HelloWorld ></HelloWorld>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import {reactive, ref, onMounted} from 'vue'
 export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  setup(){
+	  const write = ref(null)
+	  const getwrite = function(){
+		  console.log(write.value.value)
+	  }
+   onMounted(()=>{
+     
+	 
+   })
+	  
+	  return {write,getwrite}
+  },
 }
 </script>
+
+<style lang="less">
+	.home{
+		width: 500px;height: 500px;
+		margin: 0 auto;
+		background: gray;
+		display: flex;
+		justify-content: start;
+		flex-direction: column;
+		text-align: center;
+		h3{
+			color: rgb(234,215,215);
+			font-size: 20px;
+			margin-top: 20px;
+		}
+		.info{
+			width: 100%;
+			height: 30px;
+			margin-top: 5px;
+			display: flex;
+		}
+		input{
+			height: 100%;
+			flex: 1;
+			border: none;
+		}
+		.btn{
+			width: 50px;
+			border: none;
+			border-radius: 20px;
+			background: ;
+		}
+	}
+</style>
